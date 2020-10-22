1b5b370fa91275808973b1345d009d83dcf5fa95
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-22 13:48:49 -0400

    sample game data

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -4,4 +4,8 @@ class GameTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save game without title" do
+    game = Game.new
+    assert !game.save
+  end
 end
