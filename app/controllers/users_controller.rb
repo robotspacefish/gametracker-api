e8cb20d20d4df192dc57b01e42e68a863ade84cb
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-23 16:50:51 -0400

    use serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
     users = User.all
-    render json: users
+    render json: UserSerializer.new(users).to_serialized_json
   end
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user
+    render json: UserSerializer.new(user).to_serialized_json
   end
 end
