f0b9d7ce3e9f7727104265c1d4ee1bbe6bb168e3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-23 16:24:47 -0400

    methods to add game and find user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,26 @@ class User < ApplicationRecord
   has_many :games, through: :owned_games
 
   has_many :notes
+
+  def add_game_by_instance(game)
+    self.games << game
+  end
+
+  def self.destroy_by_username(username)
+    user = self.find_by_username(username)
+    user.destroy
+  end
+
+  def self.find_by_username(username)
+    self.find_by(username: username)
+  end
+
+  def add_owned_platform_to_game_by_title(game_title, platform_name)
+    game = self.games.find_by(title: game_title)
+
+    owned_game = self.owned_games.find_by(game_id: game.id)
+    owned_game.consoles.create!(name: platform_name)
+
+    owned_game
+  end
 end
